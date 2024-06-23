3cc6abb2cbe4e529ca31c945a2373eadb57a22a2
adrianoaraujo0 <adriano.amedeiros@hotmail.com>
2024-06-23 14:07:32 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Livros />
+</template>
+
+<script>
+import Livros from './components/livrosComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Livros
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/livrosComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>LIVROS</h1>
+</template>
+
+<script>
+
+export default{
+
+    created(){
+      axios
+      .get("https://livraria-api-linux.altislabtech.com.br/api/disponiveis")
+      .then((value)=> {
+        console.log(value)
+      })
+    }
+
+}
+</script>
